4a2b870b4b6644ec3573a8ddcc644fb215ed635a
sptyi <cjhilaman@yahoo.com>
2020-05-28 19:19:21 -0400

    Updates

---
+++ precache-manifest.112d3926c9741b352a3d6e84fa91aecd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2f18ce453b1871f51973e67fc5e85c",
+    "url": "/CJ-Hilaman/index.html"
+  },
+  {
+    "revision": "f8d78faf7128ace87771",
+    "url": "/CJ-Hilaman/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ae83a13e630dc388d19",
+    "url": "/CJ-Hilaman/static/js/2.b26eb158.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CJ-Hilaman/static/js/2.b26eb158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d78faf7128ace87771",
+    "url": "/CJ-Hilaman/static/js/main.ea3cf17e.chunk.js"
+  },
+  {
+    "revision": "046d827dc4290229042d",
+    "url": "/CJ-Hilaman/static/js/runtime-main.37caf68b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CJ-Hilaman/static/media/logo.5d5d9eef.svg"
+  }
+]);
